2ea9ed8675829dd5e62bbb19c9c43bdf597c8531
Marshall Pierce <marshall@mpierce.org>
2015-03-29 15:08:36 -0700

    Don't deploy with reduced redundancy

--- SiteConfig.groovy
+++ SiteConfig.groovy
@@ -60,7 +60,7 @@ ruby {
 
 // Deployment settings.
 s3_bucket = "varblog.org"
-s3_deploy_cmd = "s3cmd -c ${System.getenv('HOME')}/.s3cfg-varblog.org sync --acl-public --reduced-redundancy ${destination_dir}/ s3://${s3_bucket}/"
+s3_deploy_cmd = "s3cmd -c ${System.getenv('HOME')}/.s3cfg-varblog.org sync --acl-public ${destination_dir}/ s3://${s3_bucket}/"
 
 rsync_ssh_user = 'user@example.com'
 rsync_ssh_port = '22'
